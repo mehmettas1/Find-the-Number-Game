69d67a4fb41c27c41a3bc8ffc74e888255ca6c0b
mehmet <tas349873@gmail.com>
2022-06-04 22:48:09 +0300

    js

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
-let baslat = document.querySelector("#baslat");
-let tahmin = document.querySelector("#baslat");
-let sayi = document.querySelector("#baslat");
+let rsayi,sayac,hak=3;
+    let baslat=document.querySelector("#baslat");
+    let tahmin=document.querySelector("#tahmin");
+    let sayi=document.querySelector("#sayi");
+    
+    baslat.onclick=function()
+    {
+        tahmin.disabled=false;
+        rsayi=Math.round(Math.random()*100);
+        sayac=0;
+        
+    }
+    tahmin.onclick=function()
+    {
+        sayac++;
+        if(rsayi!=sayi.value && sayac>=hak)
+        {
+            window.alert("Üzgünüm hakkınız bitti");
+            tahmin.disabled=true;
+        }
+        
+        if(rsayi==sayi.value)
+        {
+            window.alert("tebrikler kazandınız.");
+        }
+        else if(sayi.value>rsayi)
+        {
+            window.alert("Daha küçük sayı girin");
+        }
+        else
+        {
+            window.alert("Daha büyük sayı girin");
+        }
+    }
 
-let rsayi,sayac,hak =3;
 
-baslat.onclick = function() {
-  tahmin.disabled = false;
 
-  rsayi= Math.round(Math.random()*100);
 
-  sayac=0;
-}
 
-tahmin.onclick = function() {
-  if(rsayi !=sayi.value && sayac>hak){
-    window.alert("Üzgünüz hakkınız bitti")
-    tahmin.disabled =true
-  }
 
-  if(rsayi==sayi.value) {
-    window.alert("Tebrikler kazandınız");
 
-  }
-  else if(sayi.value>rsayi) {
-   window.alert("Daha kücük bir sayı giriniz");
-  }
-  else (rsayi>sayi.value)
-  window.alert("Daha büyük bir sayı giriniz")
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
